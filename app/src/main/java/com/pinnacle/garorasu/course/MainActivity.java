adc09200a390d543721abc791bea2a1b2bc5dc76
parulchopra <parul@ssccglpinnacle.com>
2017-03-05 15:37:45 +0530

    extra setting menu item delete

--- app/src/main/java/com/pinnacle/garorasu/course/MainActivity.java
+++ app/src/main/java/com/pinnacle/garorasu/course/MainActivity.java
@@ -64,12 +64,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -78,10 +72,7 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -127,7 +118,7 @@ public class MainActivity extends AppCompatActivity
     }
     public void exploreOnClick(MenuItem item)
     {
-
+        getSupportActionBar().setTitle("SingleCourse");
         drawer.closeDrawer(GravityCompat.START);
         exploreFragment();
     }
